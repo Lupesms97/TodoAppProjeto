950c66073d10c0d70e1832cf19e8bb2727ca3d47
Lupesms97 <lupesms97@gmail.com>
2022-12-12 22:04:57 -0300

    Criando conexão com o banco de dados usando driver menager e o Sql connection

---
+++ src/main/java/org/Util/ConnectionFactory.java
@@ -0,0 +1,33 @@
+package org.Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoapp";
+    public static final String USER = "root";
+    public static final String PASS = "";
+
+    public static Connection getConnection(){
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro na conexão com o banco de dados",e);
+        }
+    }
+
+    public static void closeConnection(Connection connection){
+        try {
+            if(connection != null) connection.close();
+
+        }catch (Exception e) {
+            throw new RuntimeException("Error ao fechar a conexão com o banco de dados",e);
+        }
+
+    }
+
+
+}
