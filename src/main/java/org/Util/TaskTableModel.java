8ef1cfd44beb3d0e090e441fc63c7e97b75ad8a8
Lupesms97 <lupesms97@gmail.com>
2022-12-24 01:03:19 -0300

    implementando data formatada

--- src/main/java/org/Util/TaskTableModel.java
+++ src/main/java/org/Util/TaskTableModel.java
@@ -25,37 +25,54 @@ public class TaskTableModel extends AbstractTableModel {
     public String getColumnName(int columnIndex){
         return columns[columnIndex];
     }
-
     @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        switch (columnIndex) {
-            case 0 :
-                return tasks.get(rowIndex).getName();
-            case 1 :
-                return tasks.get(rowIndex).getDescription();
-            case 2 :
-                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-                return dateFormat.format(tasks.get(rowIndex).getDeadLine());
-            case 3 :
-                return tasks.get(rowIndex).isStatusTask();
-            case 4 :
-                return "";
-            case 5 :
-                return "";
-            default :
-                return"Dados não encontrados";
-        }
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return columnIndex == 3;
     }
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+         if(tasks.isEmpty()){
+            return Object.class;
+         }
+         return this.getValueAt(0,columnIndex).getClass();
+    }
+     @Override                                                                            
+     public Object getValueAt(int rowIndex, int columnIndex) {                            
+         switch (columnIndex) {                                                                 
+             case 0 -> {                                                                        
+                 return tasks.get(rowIndex).getName();                                          
+             }                                                                                  
+             case 1 -> {                                                                        
+                 return tasks.get(rowIndex).getDescription();                                   
+             }                                                                                  
+             case 2 -> {                                                                        
+                 SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");              
+                 return dateFormat.format(tasks.get(rowIndex).getDeadLine());                   
+             }                                                                                  
+             case 3 -> {                                                                        
+                 return tasks.get(rowIndex).isStatusTask();                                     
+             }                                                                                  
+             case 4, 5 -> {                                                                     
+                 return "";                                                                     
+             }                                                                                  
+             default -> {                                                                       
+                 return "Dados não encontrados";                                                
+             }                                                                                  
+         }                                                                                      
+     }                                                                                          
+                                                                                                
+     public String[] getColumns() {                                                             
+         return columns;                                                                  
+     }                                                                                          
+                                                                                                
+     public List<Task> getTasks() {                                                             
+         return tasks;                                                                    
+     }                                                                                          
+                                                                                                
+     public void setTasks(List<Task> tasks) {                                                   
+         this.tasks = tasks;                                                              
+     }                                                                                          
+                                                                                                
 
-    public String[] getColumns() {
-        return columns;
-    }
+       }
 
-    public List<Task> getTasks() {
-        return tasks;
-    }
-
-    public void setTasks(List<Task> tasks) {
-        this.tasks = tasks;
-    }
-}
